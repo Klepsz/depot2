da28f669259b2331a235fe1baf07254e876ecc6b
Klepsz <sklerenchyma@gmail.com>
2014-10-07 09:57:53 +0200

    counter

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,14 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+
+    def counter
+      if session[:counter].nil?
+        session[:counter] = 0
+      else
+        session[:counter] += 1
+      end    
+    end
+    @visit_count = counter
   end
 end
