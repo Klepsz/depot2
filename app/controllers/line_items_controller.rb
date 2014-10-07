65338fa96a3263362a483d04477eab9fc7036619
Klepsz <sklerenchyma@gmail.com>
2014-10-07 10:21:32 +0200

    associationg count with each product in cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -27,8 +27,9 @@ class LineItemsController < ApplicationController
   # POST /line_items.json
   def create
     product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(product: product)
-    session[:counter] = 0
+    @line_item = @cart.add_product(product.id)
+    #zerje licznik po dodaniu do koszyka
+    #session[:counter] = 0
 
     respond_to do |format|
       if @line_item.save
