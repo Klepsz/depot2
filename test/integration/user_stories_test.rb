a0d68d7666b691eeefa501c805389a9445d57b0c
Klepsz <sklerenchyma@gmail.com>
2014-10-24 09:08:24 +0200

    integration testing

--- test/integration/user_stories_test.rb
+++ test/integration/user_stories_test.rb
@@ -2,7 +2,6 @@ require 'test_helper'
 
 class UserStoriesTest < ActionDispatch::IntegrationTest
   fixtures :products
-  
   test "buying a product" do
     LineItem.delete_all
     Order.delete_all
